494aee6e6edc05798f13013c928077f6674908c5
GULOMKHODIROV <94733347+GULOMKHODIROV@users.noreply.github.com>
2023-01-26 16:21:44 +0500

    Juda katta o'zgarishlar bo'lmadi.

---
+++ Xatolar bilan ishlash.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 10:58:48 2023
+
+@author: developer
+"""
+
+#Xatolar bilan ishlash
+
+#Syntax error
+
+#print "Hello World!"
+
+#EOL va EOF xatolik
+
+#print("Hello World!
+#print("Hello World!"
+
+#IndentationError - JOY TASHLASHDA XATOLIK
+#  print("Hello Wolrd!")
+
+#print("O'ngacha sanaymiz")
+#for n in range(10):
+#print(n+1)
+
+#son = 50
+#if son>=0:
+#    print("Musbat son")
+#else:
+#print("Manfiy son")
+
+#TypeError
+#son = input("Istalgan son kiriting: ")
+#print(f"{son} ning kvadrati {son**2} ga teng")
+
+#NameError
+#prit("Hello World!")
+
+#mevalar = ['olma','uzum','nok','anor','anjir']
+#for meva in mvealar:
+ #   print(meva)
+
+#ValueError
+#son = int(input("Istalgan son kiriting: "))
+#if son>=0:
+#   print("Musbat son")
+#else:
+#    print("Manfiy son")
+
+#IndexError
+
+#mevalar = ['olma','anor','uzum']
+#print(mevalar[3])
+
+#ZeroDivisionError
+
+#x, y = 50, 50
+#z = 250/(x-y)
+
+#MANTIQIY XATOLAR
+
+#radius = 5
+#pi = 4.14
+#aylana_yuzi = pi*radius**2
+#print(aylana_yuzi)
+
+#son = float(input("Istalgan son kiriting: "))
+#ildiz = son**1/2
+#print(f"{son} ning ildizi {ildiz} ga teng")
+
+#son = float(input("Istalgan son kiriting: "))
+#ildiz = son**(1/2)
+#print(f"{son} ning ildizi {ildiz} ga teng")
+
+mevalar = ['olma','uzum','nok','anor','anjir']
+for meva in mevalar:
+    print(meva)
+print("Dastur tugadi")
+
+
+
+
